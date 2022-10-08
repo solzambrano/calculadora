19f4a99f415c38637eb78d4adbc7d5726a1d0229
soledad <solzambrano.8@gmail.com>
2022-10-08 00:57:31 -0300

    calculadora basica terminada

--- calculadora.js
+++ calculadora.js
@@ -1,34 +1,34 @@
 let butons = document.getElementsByTagName("button");
-let operations = [];
 let number = "";
 let entrada = document.getElementById("entrada");
-let special = false;
-let basic = false;
-//problema log entra al else if pero no guarda en operations ni pushea nada
+let operacion = "";
+let cut = [];
 
 recognize = (e) => {
   if (!e.target.value.match("(?=[CCE])")) {
-    if (e.target.value.match("(?=[log%π])")) special = true;
-    if (e.target.value.match("(?=[-+*/()%^π=])")) basic = true;
+    if (
+      e.target.value.match("(?=[log%π])") ||
+      e.target.value.match("(?=[-+*/()^])")
+    ) {
+      operation = e.target.value;
+    }
     number += e.target.value;
-    operations.push(e.target.value);
-    console.log(operations);
     entrada.value = number;
   } else restOperations(e);
-  if (operations.length == 2 && special) {
-    special = false;
-    entrada.value = `${number}\n ${operationsSpecials(e, operations)}`;
-  } else if (operations.length >= 3 && basic) {
-    basic = false;
-    entrada.value = number+ "\n" + resolve(operations);
+  if (e.target.value.match("=")) {
+    cut = number.split(`${operation}`);
+    console.log("si entre en =", cut);
+    cut[1] = cut[1].slice(0, -1);
+    console.log(cut[1]);
+    console.log(operation);
+    if (cut[0] == "") {
+      entrada.value = `${number} ${operationsSpecials(cut[1], operation)}`;
+    } else {
+      console.log(number);
+      entrada.value = `${number} ${resolve(operation, cut)}`;
+    }
   }
 };
-//no perder lareferencia de la operacion que estoy haciendo
-//ir concatenando el numero y cuando ingrese un simbolo recien pushear ese number al array
-//TODO revisar que todas las funciones anden bien ;
-//hacer algo diferente con el signo igual, reemplazar n/n por raiz
-//hacer un hover sobre los botones
-// TODO falla poner mas de 3 numeros o un numero>2 cifras par operar
 
 erase = () => {
   number = "";
@@ -43,35 +43,35 @@ restOperations = (e) => {
       break;
     case "C":
       entrada.value = entrada.value.slice(0, -1);
-      operations.pop();
+      cut.pop();
       if (entrada.value == "") erase();
       break;
   }
 };
-operationsSpecials = (e, op) => {
-  switch (op[0]) {
+operationsSpecials = (num, op) => {
+  switch (op) {
     case "log":
-      return Math.log(op[1]) / Math.log(10);
+      return Math.log(num) / Math.log(10);
     case "%":
-      return op[1] / 100;
+      return num / 100;
     case "π":
-      return op[1] * 3.141583104326456;
+      return num * 3.141583104326456;
   }
 };
-resolve = (op) => {
-  switch (op[1]) {
+resolve = (op, num) => {
+  switch (op) {
     case "+":
-      return Number(op[0]) + Number(op[2]);
+      return Number(num[0]) + Number(num[1]);
     case "-":
-      return op[0] - op[2];
+      return num[0] - num[1];
     case "*":
-      return op[0] * op[2];
+      return num[0] * num[1];
     case "/":
-      return op[0] / op[2];
+      return num[0] / num[1];
     case "^":
       let result = 1;
-      for (let i = 0; i < op[2]; i++) {
-        result = op[0] * result;
+      for (let i = 0; i < num[1]; i++) {
+        result = num[0] * result;
       }
       return result;
   }
@@ -80,3 +80,8 @@ resolve = (op) => {
 for (let i = 0; i < butons.length; i++) {
   butons[i].addEventListener("click", recognize);
 }
+
+//TODO agregar controles para que no se apriete igual mas de una vez
+//division por cero
+//aplicar la raiz
+//y para la version 2, hacer operaciones algebraicas
